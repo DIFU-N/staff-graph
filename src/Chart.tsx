a9fde2cfedb5a2e0f1c316d0c941e376c1ba2e2d
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-26 14:15:19 +0100

    bar chart

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,8 +1,58 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { faker } from "@faker-js/faker";
+import { Bar } from "react-chartjs-2";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+//   maintainAspectRatio: false, // this is to customize height and width
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = ["Staff"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Female",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Male",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
 const Chart = () => {
-  return (
-    <div>Chart</div>
-  )
-}
+  return <Bar options={options} data={data} width={100} height={50} />;
+};
 
-export default Chart
+export default Chart;
